126244479e61ec73d87bb19de6855a4848cd5a3b
Leyan529 <u0151057@nkfust.edu.tw>
2018-08-08 16:39:07 +0800

    進階型態繼承練習

---
+++ TypeQualify/TypeQualify/TypeQualifyTest/TestMutable.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "TestMutable.h"
+
+
+
+TestMutable::TestMutable(int v = 0) {
+	value = v;
+}
+int TestMutable::getValue() const
+{
+	return value++; // increments value
+}
+
